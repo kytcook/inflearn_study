f33f7be404ff22933c9703497b625a932f3e8af4
kytcook <kytcook@naver.com>
2023-05-24 21:52:53 +0900

    springBasic_firstCommit

--- hello2_springBasic/src/test/java/hello/core/CoreApplicationTests.java
+++ hello2_springBasic/src/test/java/hello/core/CoreApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package hello.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CoreApplicationTests {
 
 	@Test
 	void contextLoads() {
