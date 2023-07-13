1b02d2e093679f2b3d3cc6dff0baa189756dfc35
kytcook <kytcook@naver.com>
2023-07-13 22:21:16 +0900

    xmlBeanRegistration

--- hello2_springBasic/src/main/java/hello/core/AppConfig.java
+++ hello2_springBasic/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,7 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
